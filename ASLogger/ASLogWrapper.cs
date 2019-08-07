9413d9bb7c6da931f42f50ab0425080858ba1f0f
saravanan.n <saravanan.n@payoda.com>
2019-08-07 21:50:18 +0530

    initial commit

---
+++ ASLogger/ASLogWrapper.cs
@@ -0,0 +1,69 @@
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.Http;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ASLogger
+{
+    public class ASLogProvider : IAslogger
+    {
+        public static Logger logg;
+        public ASLogProvider(string serverURL)
+        {
+            //logg = new LoggerConfiguration()              
+            //   .WriteTo.Http(serverURL, httpClient: new CustomHttpClient())
+            //   .CreateLogger();
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            logg = new LoggerConfiguration()
+            .WriteTo.File(path + @"logs\Aslog.txt", rollingInterval: RollingInterval.Day)
+            .WriteTo.Http(serverURL, httpClient: new CustomHttpClient())
+            .CreateLogger();
+            logg.Debug("from Logger");
+        }
+
+        public void Debug(string msg, params object[] param)
+        {
+            logg.Debug(msg, param);
+        }
+
+        public void Error(Exception e, string msg, params object[] param)
+        {
+            logg.Error(e, msg, param);
+        }
+
+        public void Error(string msg)
+        {
+            logg.Debug(msg);
+        }
+
+        public void Info(string msg, params object[] param)
+        {
+            logg.Information(msg, param);
+        }
+    }
+
+    public interface IAslogger
+    {
+        void Debug(string msg, params object[] param);
+        void Info(string msg, params object[] param);
+        void Error(Exception e, string msg, params object[] param);
+        void Error(string msg);
+    }
+
+    public class CustomHttpClient : IHttpClient
+    {
+        private readonly HttpClient httpClient;
+
+        public CustomHttpClient()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", "secret-api-key");
+        }
+
+        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content) => httpClient.PostAsync(requestUri, content);
+
+        public void Dispose() => httpClient?.Dispose();
+    }
+}
